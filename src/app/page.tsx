50cfacab4aca396e7d2553ad0f29cd72c46b3314
John <sozhran@gmail.com>
2024-04-07 19:09:35 +0200

    it's rough but works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,83 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [easterState, setEasterState] = useState<boolean>(false);
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [currentImg, setCurrentImg] = useState<string>("bgdefault");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const easterStateSwapper = () => {
+        if (!easterState) {
+            setEasterState(!easterState);
+            setCurrentIndex(1);
+            setCurrentImg("bgimg1");
+        }
+        if (easterState) {
+            setEasterState(!easterState);
+            setCurrentIndex(0);
+            setCurrentImg("bgdefault");
+        }
+    };
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    useEffect(() => {
+        if (easterState) {
+            const intervalId = setInterval(() => {
+                if (currentIndex > 6) {
+                    easterStateSwapper();
+                } else {
+                    setCurrentIndex(currentIndex + 1);
+                    setCurrentImg("bgimg" + (currentIndex + 1).toString());
+                }
+            }, 3000);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            return () => clearInterval(intervalId);
+        }
+    }, [easterState, currentIndex]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    return (
+        <main className={currentImg}>
+            <button disabled={easterState ? true : false} onClick={easterStateSwapper}>
+                <svg id="svg1" className="egg" viewBox="0 0 500 500">
+                    <defs>
+                        <symbol id="el1" width="50" height="50" viewBox="0 0 50 50">
+                            <rect x="-5" y="-5" width="60" height="60" className="str" />
+                            <line className="str " x1="-5" y1="-5" x2="55" y2="55" />
+                            <path d="M 30 -5 L -5 30 L -5 -5 Z" className="c5 str" />
+                            <circle cx="50" cy="50" r="26" className="c6 str" />
+                            <circle cx="50" cy="50" r="16" className="c10 str" />
+                        </symbol>
+                        <symbol id="big1" width="100" height="100" viewBox="-50 -50 100 100">
+                            <use width="50" height="50" href="#el1" transform="" />
+                            <use width="50" height="50" href="#el1" transform="scale(-1, -1)" />
+                            <use width="50" height="50" href="#el1" transform="scale(-1, 1)" />
+                            <use width="50" height="50" href="#el1" transform="scale(1, -1)" />
+                        </symbol>
+                        <pattern id="pat1" width="56" height="56" viewBox="0 0 200 200" patternUnits="userSpaceOnUse">
+                            <use x="0" y="0" width="100" height="100" href="#big1" className="c1" />
+                            <use x="100" y="0" width="100" height="100" href="#big1" className="c3" />
+                            <use x="0" y="100" width="100" height="100" href="#big1" className="c4" />
+                            <use x="100" y="100" width="100" height="100" href="#big1" className="c2" />
+                        </pattern>
+                    </defs>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                    <ellipse className="str2" cx="250" cy="250" rx="200" ry="246" fill="url(#pat1)"></ellipse>
+                    <text
+                        className="str2"
+                        x="50%"
+                        text-anchor="middle"
+                        y="320"
+                        width="100%"
+                        font-family="times"
+                        font-weight="bold"
+                        font-size="194"
+                        fill="white"
+                    >
+                        ХВ
+                    </text>
+                </svg>
+            </button>
+        </main>
+    );
 }
